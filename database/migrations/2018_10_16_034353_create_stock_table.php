ecf4c8cac54038bc475d62b3500bab8ac91185f2
tomngocs <tom.ngo.cs@gmail.com>
2018-10-16 14:46:56 +1100

    Recommit 1

---
+++ database/migrations/2018_10_16_034353_create_stock_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStockTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock', function (Blueprint $table) {
+            $table->string('Name', 250);
+            $table->float('Price');
+            $table->integer('Bedrooms');
+            $table->integer('Bathrooms');
+            $table->integer('Storeys');
+            $table->integer('Garages');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('stock');
+    }
+}
