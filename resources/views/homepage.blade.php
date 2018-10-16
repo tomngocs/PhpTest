438fa0977ff050e6dc62581ef546e75eede01314
tomngocs <tom.ngo.cs@gmail.com>
2018-10-16 15:03:31 +1100

    View commit

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,3 +5,63 @@
 	<script src="js/jquery.min.js"></script>
 	<link rel="stylesheet" href="css/bootstrap.min.css" />
 	<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<br />
+	<div class="container box">
+		<h3 align="center">PHP Laravel Search</h3><br />
+		<div class="panel panel-default">
+			<div class="panel-heading">Search Customer Data</div>
+			<div class="panel-body">
+				<div class="form-group">
+					<input type="text" name="search" id="search" class="form-control" placeholder="Search Customer Data" />
+				</div>
+				<div class="table-responsive">
+					<h3 align="center">Total Data : <span id="total_records"></span></h3>
+          			<table class="table table-striped table-bordered">
+          				<thead>
+              				<tr>
+              					<th>Customer Name</th>
+				                <th>Address</th>
+				                <th>City</th>
+				                <th>Postal Code</th>
+				                <th>Country</th>
+				            </tr>
+				        </thead>
+            			<tbody>
+
+            			</tbody>
+            		</table>
+        		</div>
+      		</div>    
+    	</div>
+  	</div>
+</body>
+</html>
+
+<script>
+$(document).ready(function(){
+
+	fetch_customer_data();
+
+  	function fetch_customer_data(query = '')
+  	{
+	    $.ajax({
+	      	url:"{{ route('db_search.searchDB') }}",
+	      	method:'GET',
+	      	data:{query:query},
+	      	dataType:'json',
+	      	success:function(data)
+	      	{
+	        	$('tbody').html(data.stock_data);
+	        	$('#total_records').text(data.total);
+	      	}
+	    })
+  	}
+
+  	$(document).on('keyup', '#search', function(){
+    	var query = $(this).val();
+    	fetch_customer_data(query);
+  	});
+});
+</script>
