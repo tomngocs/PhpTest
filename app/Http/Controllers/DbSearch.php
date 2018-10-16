9a41d301564e1655f8f80c2cf80d77840f1dbc35
tomngocs <tom.ngo.cs@gmail.com>
2018-10-16 14:14:14 +1100

    Set route

--- app/Http/Controllers/DbSearch.php
+++ app/Http/Controllers/DbSearch.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use DB;
 
-class DbSearchController extends Controller
+class DbSearch extends Controller
 {
     public function getHome()
     {
